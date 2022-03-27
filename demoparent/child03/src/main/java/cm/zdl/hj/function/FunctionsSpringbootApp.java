332d90e779065db44c5d1db876a945f6323a1a78
ZHOU1989 <15710074831@163.com>
2022-03-27 21:14:53 +0800

    commit -m '第二次提交'

--- demoparent/child03/src/main/java/cm/zdl/hj/function/FunctionsSpringbootApp.java
+++ demoparent/child03/src/main/java/cm/zdl/hj/function/FunctionsSpringbootApp.java
@@ -2,8 +2,10 @@ package cm.zdl.hj.function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FunctionsSpringbootApp {
     public static void main(String[] args) {
         SpringApplication.run(FunctionsSpringbootApp.class, args);
--- demoparent/springboot/src/main/java/com/zdl/hj/SpringBootApplicationMain.java
+++ demoparent/springboot/src/main/java/com/zdl/hj/SpringBootApplicationMain.java
@@ -2,9 +2,11 @@ package com.zdl.hj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringBootApplicationMain {
 
 	public static void main(String[] args) {
