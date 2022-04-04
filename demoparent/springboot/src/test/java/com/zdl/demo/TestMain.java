260c47951bbb450e8718d39dca67e3f3473c4d47
ZHOU1989 <15710074831@163.com>
2022-04-04 23:11:11 +0800

    commit -m 'web开发环境搭建'

--- demoparent/springboot/src/test/java/com/zdl/demo/TestMain.java
+++
@@ -1,8 +0,0 @@
-package com.zdl.demo;
-
-public class TestMain {
-    public static void main(String[] args) {
-        String strDemo = "\" " +"ok" + "\" "+1234;
-        System.out.print(strDemo);
-    }
-}
