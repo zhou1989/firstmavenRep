608f9f1dcbb3fda72f155e3b6eb41ebcf32f236f
ZHOU1989 <15710074831@163.com>
2022-04-10 20:53:13 +0800

    commit -m 'web开发环境搭建'

--- demoparent/child03/src/main/java/cm/zdl/hj/function/FunctionsSpringbootApp.java
+++ demoparent/child03/src/main/java/cm/zdl/hj/function/FunctionsSpringbootApp.java
@@ -2,13 +2,19 @@ package cm.zdl.hj.function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableFeignClients
+@EnableFeignClients
 public class FunctionsSpringbootApp {
     public static void main(String[] args) {
-        SpringApplication.run(FunctionsSpringbootApp.class, args);
-        System.out.print( "******the application seccuss on ！！！******");
+        try{
+            SpringApplication.run(FunctionsSpringbootApp.class, args);
+            System.out.print( "******the application seccuss on ！！！******");
+        }catch (Exception e){
+            System.out.print( "[******zdlzdlzdl!!!!!!!!!!!!*cskjbcksdncdkd*****"+e.getMessage()+"*************hdoc**********************]");
+        }
+
 
     }
 
--- demoparent/springboot/src/main/java/com/zdl/hj/SpringBootApplicationMain.java
+++ demoparent/springboot/src/main/java/com/zdl/hj/SpringBootApplicationMain.java
@@ -2,10 +2,11 @@ package com.zdl.hj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
-//@EnableFeignClients
+@EnableFeignClients
 public class SpringBootApplicationMain {
 
 	public static void main(String[] args) {
