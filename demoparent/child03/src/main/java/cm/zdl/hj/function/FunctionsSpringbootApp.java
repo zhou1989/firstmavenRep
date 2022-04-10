67ef6d9a3968fc459c412a9c85f6994676241da8
ZHOU1989 <15710074831@163.com>
2022-04-10 23:10:08 +0800

    commit -m 'web开发环境搭建'

--- demoparent/child03/src/main/java/cm/zdl/hj/function/FunctionsSpringbootApp.java
+++ demoparent/child03/src/main/java/cm/zdl/hj/function/FunctionsSpringbootApp.java
@@ -1,11 +1,15 @@
 package cm.zdl.hj.function;
 
+import cm.zdl.hj.config.CustomConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+
 @EnableFeignClients
+@SpringBootApplication
 public class FunctionsSpringbootApp {
     public static void main(String[] args) {
         try{
--- demoparent/springboot/src/main/java/com/zdl/hj/SpringBootApplicationMain.java
+++ demoparent/springboot/src/main/java/com/zdl/hj/SpringBootApplicationMain.java
@@ -2,11 +2,12 @@ package com.zdl.hj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringBootApplicationMain {
 
 	public static void main(String[] args) {
